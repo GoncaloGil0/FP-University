de667ea6ba5009dcd4b67c8a09c89390f41f36ed
Gonçalo Gil <43255636+GoncaloGil0@users.noreply.github.com>
2020-11-27 14:19:34 +0000

    Update Ficha_5_parte_2_ex_1.c

--- Ficha_5_parte_2_ex_1.c
+++ Ficha_5_parte_2_ex_1.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 #include "myIntLib.h"
 
+//Tanto a header como o ficheiro .c com as funções encontam-se juntos com o resto dos ficheiros com o nome de:
+// (.c) Ficha_5_parte_2_ex_1.c 
+// (header.h ) Ficha_5_parte_2_ex_1.h
+
+
 int main() {
     
     int valor_max = 20, valor_minimo = 0;
